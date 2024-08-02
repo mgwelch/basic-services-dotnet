148dc6ed6300a3583d94fabb811bfb8deb687d14
Michael Welch <michaelgwelch@gmail.com>
2024-08-02 13:52:37 -0500

    initial site

--- TestClient/PubConstant.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MetasysServices_TestClient
-{
-    public static class PubConstants
-    {
-        public const string HOST_IP = "";
-        public const string USER_NAME = "";
-        public const string PASSWORD = "";
-    }
-}
